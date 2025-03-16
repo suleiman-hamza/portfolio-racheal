f7bcc9f280d23e8921e66755e20740c63e1c3fb4
suleiman hamza <hamzasuleiman530@gmail.com>
2025-03-16 13:41:37 +0100

    contact component structure

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,11 +1,64 @@
 <template>
     <section class="contact">
-      <div class="contact-left">
-        <h3>Ready to bring your ideas to life?</h3>
-        <p>Let's connect and make it happen.</p>
+      <div class="contact-top">
+        <div class="hr-line">
+          <hr>
+          <h5>Contact</h5>
+        </div>
+        <h3>Ready to bring your ideas to life? <span>Contact Me</span></h3>
       </div>
-      <form class="contact-form">
-        <h3>Send Us A Message</h3>
-      </form>
+      <div class="contact-form">
+        <div class="left-contact">
+          <span class="phone">
+            <img src="" alt="">
+            <span>
+              <h5>Number</h5>
+              <h5>+2349165486229</h5>
+            </span>
+          </span>
+          <span class="email">
+            <img src="" alt="">
+            <span>
+              <h5>Email</h5>
+              <h5>alogberacheal@gmail.com</h5>
+            </span>
+          </span>
+          <span class="location">
+            <img src="" alt="">
+            <span>
+              <h5>Location</h5>
+              <h5>14 Gynoscope Drive, New Layout Rukpakulusi.</h5>
+            </span>
+          </span>
+        </div>
+        <form>
+          <div>
+            <h4>Send Me A Message</h4>
+            <div>
+              <label for="name">
+                <span>Name</span>
+                <input type="text" id="name">
+              </label>
+              <label for="email">
+                <span>Email</span>
+                <input type="email" id="email">
+              </label>
+            </div>
+            <div>
+              <label for="message">Message</label>
+              <textarea name="message" id="message"></textarea>
+            </div>
+            <button>Contact Me</button>
+          </div>
+        </form>
+      </div>
     </section>
 </template>
+
+<style lang="css" scoped>
+.contact {
+  background-color: #fff;
+  padding: 1rem;
+  border: 1px solid red;
+}
+</style>
